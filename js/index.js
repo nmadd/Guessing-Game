c7fde10292aa86e7cbcaa34ee1ed9f42f3045a6d
nmadd <ncmaddrey@gmail.com>
2015-10-16 14:24:08 -0400

    Project refactored and (mostly) finished

--- js/index.js
+++ js/index.js
@@ -6,68 +6,69 @@ var guesses = [];
 var generateRandNum = function (){
   target = Math.floor(Math.random() * 100 + 1);
 };
+//Creates empty 'previous guess' variable to track the last guess
 var prevGuess = 0;
-var prevPrevGuess = 0;
 
-//Generates random number
+//Generates random number needed to start game
 generateRandNum();
 
+//Updates 'previous guess' variable
 var updatePrevGuess = function(){
   prevGuess = guesses[guesses.length - 1];
-  prevPrevGuess = guesses[guesses.length - 2];
 };
 
-var guessingGame = function(){
-    guesses.push($('#number-input').val());
-    updatePrevGuess();
-    setLowHigh();
-    hotOrCold();
-    inputValidator();
-    document.getElementById("guessing-form").reset();
-    guessTracker();
-    displayPrevGuesses();
-    repeatChecker();
-
-    console.log(target);
-    console.log(guesses);
-    console.log(prevGuess);
- 
-};
-
-//Resets the game and starts over
-var startOver = function(){
-  guesses = [];
-  generateRandNum();
-  resetBackground('white');
-  $('#prev-guesses').empty();
-  changeHelperText('Enter a guess from 1-100. Guess right to win a prize!');
-  $('#guesses-remaining').text(5);
-  $("#prize-image").children('img').remove();
-
-};
-
-//Creates a hint 
-var giveHint = function(){
-  var strTarget = target.toString();
-  var hintNum = '';
-  if(strTarget.length == 1){
-    hintNum = 'single digits.';
-  } else if(strTarget.length == 2){
-    hintNum = target.toString()[0] + '0s.';
+//Checks to make sure the input is valid. If it is, runs the game
+var inputValidator = function(){
+  //Pushes guess into guesses array
+  guesses.push($('#number-input').val());
+  //Updates the 'previous guess' variable
+  updatePrevGuess();
+  //Creates variable used to check if guess is a repeat
+  var repeat = false;
+  //Loops through guesses array, checking the last guess again all other guesses in array. If the guess is a repeat, changes the repeat variable to 'true'
+  for(var i =0; i < guesses.length -1; i++){
+    if(guesses[i] === guesses[guesses.length -1]){
+      repeat = true;
+    }
   }
-  alert("Here's a hint...the number is somehwere in the " + hintNum);
+  //Parses the previous guess variable from a string to a number
+  var num = parseInt(prevGuess);
+  //If the guess is out of range, or has been previously guesses, the player is prompted to guess again. If not, the game continues
+  if(num < 0 || num > 100){
+    alert('Whoooa sorry that number is out of range! Try guessing again');
+    resetInputBox();
+    //Removes last guess
+    guesses.splice(-1,1);
+  } else if(repeat == true){
+    alert('Wait you already guessed that! Guess again');
+      //Removes last guess
+      guesses.splice(-1,1);
+      resetInputBox();
+  } else {
+    hotOrCold();
+  }
 };
 
-//Pushes guess to 'guesses' array when 'submit' button is clicked
-$('#submit-btn').on('click', guessingGame);
-//Calls 'start over' function when start over button is clicked
-$('#startover-btn').on('click', startOver);
-//Calls 'give hint' function when start over button is clicked
-$('#hint-btn').on('click', giveHint);
+//Pushes guess to 'guesses' array and runs 'inputValidator' when 'submit' button is clicked
+$('#submit-btn').on('click', inputValidator);
+
+//Makes pressing the 'enter' key also run 'inputValidator' (so users can either hit 'submit' button or press the 'enter' key)
+$("input").keypress(function(event) {
+    if (event.which == 13) {
+        event.preventDefault();
+        inputValidator();
+    }
+});
+
 
 //Resets the background color
 var resetBackground = function(color){
   $('.background').css('background-color', color);
+};
+
+//Changes 'helper text' at the top of the page
+var changeHelperText = function(text){
+  $('#helper-text').text(text);
 };
 
 //Creates hot and cool color variables, to be used as background colors
@@ -77,9 +78,18 @@ var burningUp = '#FF0000';
 var iceCold = '#C4EFFF';
 var warmRed = '#FC6868';
 
-//Lets player know whether they're hot or cold and changes the background color accordingly
+//Lets player know whether they're hot or cold and runs resetInputBox, guessTracker, and displayPrevGuesses
 var hotOrCold = function(){
+  //Creates empty variable that changes depending on whether the player's guess is low or high
+  var lowHigh = '';
+  //Creates a variable that gets the absolute distance between the player's guess and the target #
   var range = Math.abs(target - prevGuess);
+  //Sets the 'lowHigh' variable
+  if(prevGuess < target)
+    lowHigh = 'higher.';
+  if(prevGuess > target)
+    lowHigh = 'lower.';
+  //Lets user know whether they are hot or cold, based on how far their guess was from the target
   if(range >0 && range <=5){
     resetBackground(burningUp);
     changeHelperText("You're BURNING UP! Try guessing a little " + lowHigh);
@@ -101,49 +111,23 @@ var hotOrCold = function(){
     resetBackground(iceCold);
     changeHelperText("Brrr...you're ice cold. Try guessing a lot " + lowHigh);
   }
+
+    resetInputBox();
+    guessTracker();
+    displayPrevGuesses();
+
+    console.log(target);
+    console.log(guesses);
+    console.log(prevGuess);
 };
 
-//Changes text at the top of the page
-var changeHelperText = function(text){
-  $('#helper-text').text(text);
+
+//Resets guess input box
+var resetInputBox = function(){
+  document.getElementById("guessing-form").reset();
 };
 
-//Creates and sets variable to inform player whether their guess is low or high
-var lowHigh = '';
-var setLowHigh = function(){
-  if(prevGuess < target)
-    lowHigh = 'higher.';
-  if(prevGuess > target)
-    lowHigh = 'lower.';
-};
-
-//Generates a 'prize' and alerts the winner they've won
-var prizeGenerator = function(){
-  //Generate random number 1-3
-  var randNum = Math.floor(Math.random() * 3);
-  var prizes =[{name:'Tamagotchi', link: "<img id='theImg' src='http://php.scripts.psu.edu/dmh5086/t/i/shells/v4/orange_stars.jpg'/>"},
-  {name:'Pet Rock', link: "<img id='theImg' src='http://a.abcnews.go.com/images/Lifestyle/gty_pet_rock_150401_4x3_992.jpg'/>"},
-  {name:'Honeybadger', link: "<img id='theImg' src='http://i.ytimg.com/vi/x9Jr9JKpsX8/maxresdefault.jpg'/>"}]
-  alert('You win! Click OK to see your prize');
-      resetBackground('white');
-      $("#prize-image").append(prizes[randNum].link);
-      $('#prev-guesses').empty();
-      changeHelperText('Congratulations! You won a ' + prizes[randNum].name);
-      
-}
-
-//Displays the players' previous guesses
-var displayPrevGuesses = function(){
-  var guessStr ='';
-  if(guesses.length >= 1){
-     for(var i =0; i < guesses.length; i++){
-      guessStr += guesses[i] + ' ';
-     }
-    $('#prev-guesses').text('Your previous guesses are: \n' +guessStr);
-  }
-};
-
-//Tracks amount of guesses
+//Tracks amount of guesses and alerts player if game is over
 var guessTracker = function(){
   var guessNum = guesses.length;
   //Changes 'Guesses Remaining' number
@@ -160,125 +144,78 @@ var guessTracker = function(){
   }
 };
 
-//Checks if guess has already been made. If it has been, removes that guess and prompts the users to guess again
-var repeatChecker = function(){
-  for(var i =0; i < guesses.length -1; i++){
-    if(guesses[i] === guesses[guesses.length -1]){
-      alert('Wait you already guessed that! Guess again');
-      //Removes last guess
-      guesses.splice(-1,1);
-    }
+//Displays the players' previous guesses
+var displayPrevGuesses = function(){
+  var guessStr ='';
+  //If there's only been one guess, displays that # as 'previous guess'
+  if(guesses.length == 1){
+     for(var i =0; i < guesses.length; i++){
+      guessStr += guesses[i] + ' ';
+     }
+    $('#prev-guesses').append("<p id='prev-guess-text'>Your previous guesses are: \n" +guessStr + "</p>");
+  //If there have been more than one guesses, displays each guess with a comma separating each
+  } else if(guesses.length > 1){
+    for(var i =0; i < guesses.length; i++){
+      guessStr += guesses[i];
+      if(i < guesses.length - 1){
+        guessStr += ', ';
+      }
+     }
+    $('#prev-guess-text').text("Your previous guesses are: \n" +guessStr);
   }
 };
 
-//Checks to make sure the input is between 1 and 100
-var inputValidator = function(){
-  var num = parseInt(prevGuess);
-  if(num < 0 || num > 100){
-    alert('Whoooa sorry that number is out of range! Try guessing again');
-    //Removes last guess
-    guesses.splice(-1,1);
+
+
+//Resets the game and starts over
+var startOver = function(){
+  guesses = [];
+  generateRandNum();
+  resetBackground('white');
+  $(".hide-me").show();
+  $('#prev-guesses').empty();
+  changeHelperText('Enter a guess from 1-100. Guess right to win a prize!');
+  $('#guesses-remaining').text(5);
+  $("#prize-image").children('img').remove();
+};
+
+//Creates a hint 
+var giveHint = function(){
+  var strTarget = target.toString();
+  var hintNum = '';
+  if(strTarget.length == 1){
+    hintNum = 'single digits.';
+  } else if(strTarget.length == 2){
+    hintNum = target.toString()[0] + '0s.';
+  } else if(strTarget.length == 3){
+    hintNum = target.toString()[0] + '00s.';
   }
+  alert("Here's a hint...the number is somehwere in the " + hintNum);
 };
 
-/*
+//Calls 'start over' function when start over button is clicked
+$('#startover-btn').on('click', startOver);
+//Calls 'give hint' function when start over button is clicked
+$('#hint-btn').on('click', giveHint);
 
-//Tracks whether user is hot or cold
-var isWarm = false;
-var isHot = false;
-var isBurningUp = false;
-var isCold = false;
-var isIceCold = false;
-
-//Resets temperature variables
-var tempResetter = function(){
-  isWarm = false;
-  isHot = false;
-  isBurningUp = false;
-  isCold = false
-  isIceCold = false;
-};
-
-*/
-
-/*
-var higherOrLower = function(){
-  if(isWarm){
-    changeHelperText("You're warming up! Try guessing " + lowHigh);
-  };
-  if(isHot){
-    changeHelperText("You're getting HOT! Try guessing " + lowHigh);
-  };
-  if(isBurningUp){
-    changeHelperText("You're BURNING UP! Try guessing a little " + lowHigh);
-  };
-  if(isIceCold){
-    changeHelperText("Brrr...you're ice cold. Try guessing a lot " + lowHigh);
-  };
-   if(isCold){
-    changeHelperText("It's chilly in here... Try guessing " + lowHigh);
-  };
-};
-
-*/
-
-/*var changeHelperTextCreator = function(text){
-  return function(){
-    $('#helper-text').text(text);
-  }:
-};
-
-var changeHelperText = changeHelperTextCreator(" Your guess of " + guesses[guesses.length-1]+ " was a little high. Guess lower!")
-
-
-
-var higherOrLower = function(){
-  if(guesses[guesses.length -1] > target){
-    changeHelperText(" Your guess of " + guesses[guesses.length-1]+ " was a little high. Guess lower!");
-  };
-  if(guesses[guesses.length -1] < target){
-    changeHelperText('Your guess of ' + guesses[guesses.length-1]+ ' was a little low. Guess higher!');
-  };
-};
-
-*/
-
-/*
-//Changes background color depending on whether user is hot or cold
-var bgdChange = function(){
-  if(isHot === true){
-    resetBackground(hotRed);
-    
-  };
-  if(isCold === true){
-    resetBackground(coolBlue);
-    
-  }; 
-  if(isBurningUp === true){
-    resetBackground(burningUp);
-   
-  }; 
-  if(isWarm === true){
-    resetBackground(warmRed);
-  
-  };
-  if(isIceCold === true){
-    resetBackground(iceCold);
-   
-  };  
-};
-*/
-
-/*
-//Checks for winner
-var winChecker = function(){
-    if(prevGuess == target){
-      alert('You win! Click OK to see your prize');
+//Generates a 'prize' and alerts the winner they've won
+var prizeGenerator = function(){
+  //Generate random number 1-3
+  var randNum = Math.floor(Math.random() * 5);
+  //Creates a 'prizes' object
+  var prizes =[{name:'Tamagotchi', link: "<img id='theImg' src='http://php.scripts.psu.edu/dmh5086/t/i/shells/v4/orange_stars.jpg'/>", text: " Remember to change its diapers."},
+  {name:'Pet Rock', link: "<img id='theImg' src='http://a.abcnews.go.com/images/Lifestyle/gty_pet_rock_150401_4x3_992.jpg'/>", text: " Be sure to feed it twice a day."},
+  {name:'Honeybadger', link: "<img id='theImg' src='http://i.ytimg.com/vi/x9Jr9JKpsX8/maxresdefault.jpg'/>", text: " You're in luck...they love to cuddle."},
+  {name:'Dog Snuggie', link: "<img id='theImg' src='http://ep.yimg.com/ay/villagestreetwear/snuggie-for-dogs-the-only-blanket-with-sleeves-for-dogs-56.jpg'/>", text: ' Because what do dogs love more than blankets with sleaves?'},
+  {name:'Lifetime supply of Pogs', link: "<img id='theImg' src='http://www.thingsofmyinterest.com/wp-content/uploads/2011/06/Pogs_01.jpg'/>", text: " Oh snap!" }]
+  alert('You win! Click OK to see your prize');
       resetBackground('white');
-      $("#prize-image").append("<img id='theImg' src='http://php.scripts.psu.edu/dmh5086/t/i/shells/v4/orange_stars.jpg'/>");
-    }
-};
-*/
+      $("#prize-image").append(prizes[randNum].link);
+      $('#prev-guesses').empty();
+      $(".hide-me").hide();
+      changeHelperText('Congratulations! You won a ' + prizes[randNum].name + '.' + prizes[randNum].text);
+      
+}
 
 
 
